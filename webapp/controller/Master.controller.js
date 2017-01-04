18439348ff91951755c561dbb36757637f7e260f
gmahi <gullamahi@gmail.com>
2017-01-04 22:47:26 +0000

    twoway binding

--- webapp/controller/Master.controller.js
+++ webapp/controller/Master.controller.js
@@ -1,45 +1,53 @@
 sap.ui.define(["sapui5/demo/odata/readingdata/bestpractice/controller/BaseController"
 
-], function(BaseController) {
+		], function(BaseController) {
 
-	"use strict";
-	return BaseController.extend("sapui5.demo.odata.readingdata.bestpractice.controller.Master", {
-		_getPathInfo: function() {
-			if (!this._sPath) {
-				var oPage = this.getView().byId("productInfo");
-				// "/Products(1)/ProductName
-				this._sPath = oPage.getBindingContext()
-					.getPath() + "/ProductName";
-			}
-			return this._sPath;
-		},
+			"use strict";
+			return BaseController.extend("sapui5.demo.odata.readingdata.bestpractice.controller.Master", {
+					_getPathInfo: function() {
+						if (!this._sPath) {
+							var oPage = this.getView().byId("productInfo");
+							// "/Products(1)/ProductName
+							this._sPath = oPage.getBindingContext()
+								.getPath();
+						}
+						return this._sPath;
+					},
 
-		onChange: function() {
-			var oModel = this.getModel();
-			var sPath = this._getPathInfo();
-			var sNewValue = this.getView()
-				.byId("newProductName").getValue();
-			if (!sNewValue) {
-				return;
-			}
+					onChange: function() {
+						var oModel = this.getModel();
+						var sPath = this._getPathInfo();
+						var sNewValue = this.getView()
+							.byId("newProductName").getValue();
+						if (!sNewValue) {
+							return;
+						}
 
-			oModel.setProperty(sPath, sNewValue);
-			console.log("After setProperty: Model has pending changes:",
-				oModel.hasPendingChanges());
-			console.log("Pending changes:",
-				oModel.getPendingChanges());
-		},
+						oModel.setProperty(sPath, sNewValue);
+						console.log("After setProperty: Model has pending changes:",
+							oModel.hasPendingChanges());
+						console.log("Pending changes:",
+							oModel.getPendingChanges());
+					},
 
-		onReset: function() {
-			var oModel = this.getModel();
-			var sPath = this._getPathInfo();
-			oModel.resetChanges([sPath]);
-			console.log("After reset:Model has pendingchanges:",
-				oModel.hasPendingChanges());
-			console.log("Pending changes:",
-				oModel.getPendingChanges());
-		}
+					onReset: function() {
+						var oModel = this.getModel();
+						var sPath = this._getPathInfo();
+						oModel.resetChanges([sPath]);
+						console.log("After reset:Model has pendingchanges:",
+							oModel.hasPendingChanges());
+						console.log("Pending changes:",
+							oModel.getPendingChanges());
+					},
+					onCheck: function() {
+						var oModel = this.getModel();
+						var sPath = this._getPathInfo();
+						var oData = oModel.getData(sPath);
+						console.log("Model data:", oData);
+						console.log("Model haspendingchanges:", oModel.hasPendingChanges());
+						console.log("Pending changes:", oModel.getPendingChanges());
+					}
 
-	});
+					});
 
-});
+			});
